d54d30dfa60f97b1d1fb1854776e0aec734eaf6e
Ramees Jahan MA <101307762+RameesJahan@users.noreply.github.com>
2022-07-28 09:37:13 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+//pre loader
+window.addEventListener('load', loaderHide);
+function loaderHide() {
+  let loaderIcon = document.getElementById("loader");
+  loaderIcon.classList.add("hide");
+}
 
+function absorbEvent_(event) {
+  var e = event || window.event;
+  e.preventDefault && e.preventDefault();
+  e.stopPropagation && e.stopPropagation();
+  e.cancelBubble = true;
+  e.returnValue = false;
+  return false;
+}
+
+preventLongPressMenu(document.querySelectorAll('body img'));
+
+function preventLongPressMenu(nodes) {
+  for (var i = 0; i < nodes.length; i++) {
+    nodes[i].ontouchstart = absorbEvent_;
+    nodes[i].ontouchmove = absorbEvent_;
+    nodes[i].ontouchend = absorbEvent_;
+    nodes[i].ontouchcancel = absorbEvent_;
+  }
+}
