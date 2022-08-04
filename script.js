c1ea7d5b7fb6ad8bbde8516079aaab7ae42df169
Ramees Jahan <rameesjahan37@gmail.com>
2022-08-04 23:43:25 +0530

    Formatted Code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //pre loader
-window.addEventListener('load', loaderHide);
+window.addEventListener("load", loaderHide);
 function loaderHide() {
   let loaderIcon = document.getElementById("loader");
   loaderIcon.classList.add("hide");
 }
-
