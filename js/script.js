664e1acde29c899b40acdfdbee8f93e18b8e97c5
Rames Jahan <rameesjahan37@gmail.com>
2022-11-16 23:58:02 +0530

    Landing Page Designed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onscroll = () => {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("nav-bar").classList.add("bg-show");
+  } else {
+    document.getElementById("nav-bar").classList.remove("bg-show");
+  }
+}
+
+const navToggle = () => {
+  document.getElementById("nav-toggle").classList.toggle("nav-show");
+}
