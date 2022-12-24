96e9cfdbda5e12a998231a2ff047c767257daf43
Ramees Jahan <rameesjahan37@gmail.com>
2022-12-25 00:15:51 +0530

    Added Scroll me Button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 window.onscroll = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("nav-bar").classList.add("bg-show");
+    document.getElementById("scroll-me").classList.add("hide");
   } else {
     document.getElementById("nav-bar").classList.remove("bg-show");
+    document.getElementById("scroll-me").classList.remove("hide");
   }
 }
 
